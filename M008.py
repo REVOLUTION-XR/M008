424921795d8bc51fec6076589ea5bc9020afd639
SEFAT x MAHADI <114663032+SEFAT-MAHADI@users.noreply.github.com>
2023-05-27 17:41:02 +0400

    Update M008.py

--- M008.py
+++ M008.py
@@ -1,5 +1,5 @@
 import os
 from platform import architecture
-if architecture()[0]=='64bit':os.system('git pull;chmod +x SEFATMAHADI;./SEFATMAHADI')
+if architecture()[0]=='64bit':os.system('git pull;chmod +x MAHADISEFAT;./MAHADISEFAT')
 else:exit('\033[1;31m\n Sorry 32bit device not support ')
  
